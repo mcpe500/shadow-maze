a491510390a53e4a050461d017e7e225c47950df
mcpe500 <68177813+mcpe500@users.noreply.github.com>
2023-05-29 13:05:14 +0700

    p

--- src/shadowmaze2/character/Player.java
+++ src/shadowmaze2/character/Player.java
@@ -2,4 +2,33 @@ package shadowmaze2.character;
 
 public class Player {
 
+    private float x; // Player position x-coordinate
+    private float y; // Player position y-coordinate
+    private float speed; // Player speed
+
+    public Player(float startX, float startY, float playerSpeed) {
+        x = startX;
+        y = startY;
+        speed = playerSpeed;
+    }
+
+    public void move(float dx, float dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public void display() {
+        // Draw the player on the screen
+        // Example: ellipse(x, y, playerSize, playerSize);
+        
+    }
+
+    // Getter methods for accessing player position
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
 }
