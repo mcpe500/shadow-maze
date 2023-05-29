e89003f9a6e5b39bbe25786c23c653795649214f
mcpe500 <68177813+mcpe500@users.noreply.github.com>
2023-05-29 10:16:19 +0700

    Update: Bikin class Karakter buat polymorphis player sama enemy

--- src/shadowmaze2/utils/Karakters.java
+++ src/shadowmaze2/utils/Karakters.java
@@ -17,26 +17,32 @@ public class Karakters {
     // Getter dan setter untuk atribut-atribut di atas
     public void moveUp() {
         // Logika pergerakan ke atas
+        this.posY -= 1;
     }
 
     public void moveDown() {
         // Logika pergerakan ke bawah
+        this.posY += 1;
     }
 
     public void moveLeft() {
         // Logika pergerakan ke kiri
+        this.posX -= 1;
     }
 
     public void moveRight() {
         // Logika pergerakan ke kanan
+        this.posX += 1;
     }
 
     public void decreaseHealth(int amount) {
         // Mengurangi health point karakter
+        this.healthPoint -= amount;
     }
 
     public void increaseScore(int amount) {
         // Menambah skor karakter
+        this.score += amount;
     }
 
     public void usePortalGun() {
